84c9ff3ae81fde72bfe5a3db0096958576067631
Sofia B. Lopez <sofiabelenlopez00@gmail.com>
2020-07-09 00:04:53 +0300

    Rewrite energy graphing script

--- energy.py
+++ energy.py
@@ -1,64 +1,27 @@
-import matplotlib as mpl
-from matplotlib import rc
 import matplotlib.pyplot as plt
-import numpy as np
-import math as mt
-from scipy.optimize import curve_fit
 import pandas as pd
-from scipy.interpolate import UnivariateSpline
+import seaborn as sns
 
-trk = open('Data/counter','r')
+trk = open('Data2/counter', 'r')
 counter = int(trk.read()) - 1
-#counter = 19
 
-file_name = 'Data/energy'+str(counter)
-data = pd.read_table(file_name,sep = '\s+')
-x = data['x']
-y = data['e']
-y2 = data['k']
-y3 = data['p']
+filename = 'Data2/energy'+str(counter)
+data = pd.read_table(filename, sep=r'\s+')
 
-#x = np.array(x, dtype=np.float128)
-#y = np.array(y, dtype=np.float128)
-#
-#def line(x,a,b):
-#    return a*x+b
-#
-#popt, pcov = curve_fit(f = line, xdata = x, ydata = y)
-#sigma = np.sqrt(np.diag(pcov))
-#print("sigma energy=",sigma[0])
-#popt, pcov = curve_fit(f = line, xdata = x, ydata = y2)
-#sigma = np.sqrt(np.diag(pcov))
-#print("sigma kinetic=",sigma[0])
-#popt, pcov = curve_fit(f = line, xdata = x, ydata = y3)
-#sigma = np.sqrt(np.diag(pcov))
-#print("sigma potential=",sigma[0])
+sns.set(context='notebook', style='darkgrid')
+sns.set_palette(sns.diverging_palette(10, 220, sep=80, n=7), color_codes=True)
 
-plt.style.use('ggplot')
-font_size=35
-plt.figure(figsize=(25,14))
-plt.rc('legend', fontsize=font_size)    # legend fontsize
-plt.xticks(fontsize=font_size)
-plt.yticks(fontsize=font_size)
+fig, ax = plt.subplots()
+ax.scatter(data['time'], data['kinetic'], color='r',\
+        label='Средняя кинетическая энергия')
+ax.scatter(data['time'], data['potential'], color='g',\
+        label='Средняя потенциальная энергия')
+ax.scatter(data['time'], data['kinetic'] + data['potential'], color='b',\
+        label='Средняя полная энергия')
+ax.set_xlabel(r"Время $\left(\sqrt{\frac{m\sigma^2}{\varepsilon} }\right)$")
+ax.set_ylabel(r"Энергия $\left(\varepsilon\right)$")
+ax.set_title(r"Зависимость энергии от времени")
+ax.legend(loc='best')
 
-#plt.scatter(x,y,label=r"$\langle E\rangle$")
-#plt.scatter(x,y2,label=r"$\langle K\rangle$")
-#plt.scatter(x,y3,label=r"$\langle П \rangle$")
-
-plt.scatter(x,y,label= "Средняя полная энергия")
-#plt.plot(x, line(x, *popt),c='black')
-plt.xlabel(r"Время $\left(\sqrt{\frac{m\sigma^2}{\varepsilon} }\right)$", fontsize=font_size)
-plt.ylabel(r"Энергия $\left(\varepsilon\right)$", fontsize=font_size)
-plt.title(r"Зависимость энергии от времени", fontsize=font_size)
-plt.legend()
-
-#plt.show()
-plt.savefig("Data/energy"+str(counter)+".png")
-
-plt.scatter(x,y2,label="Средняя кинетическая энергия")
-plt.scatter(x,y3,label="Средняя потенциальная энергия")
-plt.legend()
-
-plt.savefig("Data/energy_kp"+str(counter)+".png")
-plt.savefig("../Lab2/selfD/energy_kp"+str(counter)+".png")
-#plt.show()
+fig.set_size_inches(8, 6)
+fig.savefig("Data2/energy" + str(counter) + ".png")
